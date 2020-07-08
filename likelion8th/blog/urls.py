d132f1dfe6fe3e34e9cc4db8be931222ae36cc30
sieun <se0983@kookmin.ac.kr>
2020-07-09 02:16:15 +0900

    7주차 과제

--- likelion8th/blog/urls.py
+++ likelion8th/blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('edit/<int:blog_id>', views.edit, name = "edit"), #id값 받아야되니까 <int:blog_id>패스커먼드? 써줘
     path('update/<int:blog_id>', views.update, name = "update"),
     path('delete/<int:blog_id>', views.delete, name = "delete"),
+    path('commenting/<int:blog_id>', views.commenting, name='commenting'),
+    path('like/<int:blog_id>', views.like, name='like'),
 ]
